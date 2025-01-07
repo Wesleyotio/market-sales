d5ef9c9a876df4f5d5c891644c1a43a8e6a65bec
Wesleyotio <wesleymagnos@gmail.com>
2025-01-07 18:14:04 -0300

    Updated entity product

--- backend/src/Domain/Entities/Product.php
+++ backend/src/Domain/Entities/Product.php
@@ -2,99 +2,58 @@
 
 namespace App\Domain\Entities;
 
-use DateTime;
+use DateTimeImmutable;
 
-class Product 
+readonly class Product
 {
-    private readonly int $id;
-    private int $code;
-    private readonly int $typeProductId; 
-    private string $name;
-    private float $value;
-    private readonly DateTime $createdAt;
-    private DateTime $updatedAt;
-    private DateTime $deletedAt;
-
     public function __construct(
-        int $id, 
-        int $code,
-        int $typeProductId,
-        string $name,
-        float $value, 
-        DateTime $createdAt, 
-        DateTime $updatedAt
+        private int $id,
+        private int $code,
+        private int $typeProductId,
+        private string $name,
+        private float $value,
+        private DateTimeImmutable $createdAt,
+        private DateTimeImmutable $updatedAt,
+        private ?DateTimeImmutable $deletedAt = null
     ) {
-        $this->id = $id;
-        $this->code = $code;
-        $this->typeProductId = $typeProductId;
-        $this->name = $name;
-        $this->value = $value;
-        $this->createdAt = $createdAt;
-        $this->updatedAt = $updatedAt;
-       
     }
-    
-    public function getId(): int 
+
+    public function getId(): int
     {
         return $this->id;
     }
-    
+
     public function getCode(): int
     {
         return $this->code;
     }
-    
+
     public function getTypeProductId(): int
     {
         return $this->typeProductId;
     }
-    
-    public function getName(): string 
+
+    public function getName(): string
     {
         return $this->name;
     }
-    
-    public function getValue(): float 
+
+    public function getValue(): float
     {
         return $this->value;
     }
-    
-    public function getCreatedAt(): DateTime
+
+    public function getCreatedAt(): DateTimeImmutable
     {
         return $this->createdAt;
     }
-    
-    public function getUpdatedAt(): DateTime
+
+    public function getUpdatedAt(): DateTimeImmutable
     {
         return $this->updatedAt;
     }
-    public function getDeletedAt(): DateTime
+    public function getDeletedAt(): DateTimeImmutable
     {
         return $this->deletedAt;
-    }
-
-    public function setCode(int $code): void 
-    {
-        $this->code = $code;
-    }
-    
-    public function setName(string $name): void 
-    {
-        $this->name = $name;
-    }
-    
-    public function setValue(float $value): void 
-    {
-        $this->value = $value;
-    }
-    
-    public function setUpdatedAt(DateTime $updatedAt): void 
-    {
-        $this->updatedAt = $updatedAt;
-    }
-
-    public function setDeletedAt(DateTime $deletedAt): void 
-    {
-        $this->deletedAt = $deletedAt;
     }
 }
